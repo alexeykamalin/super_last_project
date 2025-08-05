365076b65ddbc8d488b83d7908096d7f41eeb1c3
Alex <mail@alexkam.ru>
2025-08-05 20:41:39 +0700

    123456

--- script3.xg.py
+++ script3.xg.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from xgboost import XGBRegressor
 import matplotlib.pyplot as plt
-import joblib
 
 
 # === Загрузка данных ===
@@ -36,10 +35,7 @@ y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
-joblib.dump(model, 'xgboost_model.joblib')
 
 print(f"MAE: {mae:.2f}")
 print(f"RMSE: {rmse:.2f}")
 print(f"R^2: {r2:.2f}")
-
-
